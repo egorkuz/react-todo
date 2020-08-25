91dc93bdda474fb59506fbd3e65520210e045331
MagicMan <microblogworkingmail@mail.ru>
2020-08-26 01:35:49 +0300

    Updates

---
+++ precache-manifest.8a4959ea6dae5c04db90d9c4e906f79d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53c8f0e015bb1bebb5e46e497f6fc3df",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "4ad4cf446510e8d46206",
+    "url": "/react-todo/static/css/main.2357a393.chunk.css"
+  },
+  {
+    "revision": "65f2f0f6f9252488a6e2",
+    "url": "/react-todo/static/js/2.e9bb8747.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.e9bb8747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ad4cf446510e8d46206",
+    "url": "/react-todo/static/js/main.8d37afba.chunk.js"
+  },
+  {
+    "revision": "357fd4d1679917c93c59",
+    "url": "/react-todo/static/js/runtime-main.f5c96117.js"
+  }
+]);
